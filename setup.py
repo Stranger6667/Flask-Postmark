d6c1ee3ed9c42b91b144ea5c224c20147f27eeaa
Dmitry Dygalo <dadygalo@gmail.com>
2020-08-29 14:26:27 +0200

    chore: Drop Python < 3.5

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 import os
 import re
 
@@ -37,10 +36,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
