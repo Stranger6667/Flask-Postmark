fdd1259bf463bd2eef7cebcd1a82fd586fe5e117
Dmitry Dygalo <dadygalo@gmail.com>
2020-08-29 14:52:45 +0200

    refactor: Function-based tests

--- test/test_postmark.py
+++ test/test_postmark.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import pytest
-from flask import json
 
 from flask_postmark import Postmark
 
@@ -12,68 +11,64 @@ SENDER = "sender@example.com"
 DATA = {"From": SENDER, "To": RECEIVER, "Subject": SUBJECT, "HtmlBody": BODY}
 
 
-class TestPostmark:
-    @pytest.fixture(autouse=True)
-    def setup(self, test_client):
-        self.client = test_client
+def test_token(test_client, server_token):
+    assert test_client.get("/token").data == server_token
 
-    def post(self, url, data=None):
-        response = self.client.post(
-            url, data=json.dumps(data), content_type="application/json"
-        )
-        return json.loads(response.data)
 
-    def test_token(self, server_token):
-        assert self.client.get("/token").data == server_token
+def test_send(post):
+    assert post("/send", DATA) == {
+        "Attachments": [],
+        "Bcc": None,
+        "Cc": None,
+        "From": SENDER,
+        "Headers": [],
+        "HtmlBody": BODY,
+        "Metadata": None,
+        "Tag": None,
+        "TextBody": None,
+        "To": RECEIVER,
+        "TrackLinks": "None",
+        "TrackOpens": None,
+        "ReplyTo": None,
+        "Subject": SUBJECT,
+    }
 
-    def test_send(self):
-        assert self.post("/send", DATA) == {
-            "Attachments": [],
-            "Bcc": None,
-            "Cc": None,
-            "From": SENDER,
-            "Headers": [],
-            "HtmlBody": BODY,
-            "Metadata": None,
-            "Tag": None,
-            "TextBody": None,
-            "To": RECEIVER,
-            "TrackLinks": "None",
-            "TrackOpens": None,
-            "ReplyTo": None,
-            "Subject": SUBJECT,
-        }
 
-    def test_send_batch(self):
-        data = self.post("/send_batch", [DATA, DATA])
-        expected = {
-            "Attachments": [],
-            "From": SENDER,
-            "Headers": [],
-            "Subject": SUBJECT,
-            "HtmlBody": BODY,
-            "To": RECEIVER,
-        }
-        assert data == [expected, expected]
+def test_send_batch(post):
+    data = post("/send_batch", [DATA, DATA])
+    expected = {
+        "Attachments": [],
+        "From": SENDER,
+        "Headers": [],
+        "Subject": SUBJECT,
+        "HtmlBody": BODY,
+        "To": RECEIVER,
+    }
+    assert data == [expected, expected]
 
-    def test_is_same_client(self):
-        assert self.post("/is_same_client", DATA) is True
 
-    def test_empty_app(self, app):
-        assert len(app.teardown_appcontext_funcs) == 1
-        postmark = Postmark()
-        postmark.init_app(app)
-        assert len(app.teardown_appcontext_funcs) == 2
+def test_is_same_client(post):
+    assert post("/is_same_client", DATA) is True
 
-    def test_get_app(self, app):
-        postmark = Postmark(app)
-        assert postmark._get_app() is app
 
-    def test_no_context(self, app):
-        postmark = Postmark(app)
-        with pytest.raises(RuntimeError):
-            postmark.client
+def test_empty_app(app):
+    assert len(app.teardown_appcontext_funcs) == 1
+    postmark = Postmark()
+    postmark.init_app(app)
+    assert len(app.teardown_appcontext_funcs) == 2
 
-    def test_teardown(self, app):
-        postmark = Postmark(app)
-        assert postmark.teardown(None) is None
+
+def test_get_app(app):
+    postmark = Postmark(app)
+    assert postmark._get_app() is app
+
+
+def test_no_context(app):
+    postmark = Postmark(app)
+    with pytest.raises(RuntimeError):
+        postmark.client
+
+
+def test_teardown(app):
+    postmark = Postmark(app)
+    assert postmark.teardown(None) is None
